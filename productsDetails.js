c8a24a855e50e142cc5de45330f2bab99b62bb5a
packirisamykaran <43819446+packirisamykaran@users.noreply.github.com>
2022-07-22 15:21:02 +0800

    product list

---
+++ productsDetails.js
@@ -0,0 +1,44 @@
+
+
+const productDetails = {
+
+    "rebar cutter":{
+        "C-43 (10 to 41mm)":{
+            "Motor": "2.2kW",
+            "Dimensions (mm) (L x W x H)":"580 x 710 x 1,040",
+            "Net Weight (kg)":"600"
+        },
+        "C-33 (10 to 32mm)":{
+            "Motor": "1.5kW",
+            "Dimensions (mm) (L x W x H)":"555 x 615 x 920",
+            "Net Weight (kg)":"460"
+        }
+    },
+
+    "rebar bender":{
+        "TKSB-25":{
+            "Maximum Bending Diameter":"25mm",
+            "Maximum Radius": "600mm",
+            "Minimum Radius": "120mm",
+            "Power":"4Kw",
+            "Voltage":"415v / 50Hz",
+            "Net Weight (kg)": "560",
+            "Dimensions (mm) (L x W x H)":"950mm x 950mm x 900mm"
+        },
+
+        "B-16":{
+            "Electric Motor":"1.5kW",
+            "Working Speed":"50Hz – 24.260Hz – 29.1",
+            "Dimensions (mm) (L x W x H)":"690 x 740 x 855",
+            "Net Weight (kg)":"190"
+        }
+
+    }
+
+
+
+}
+
+
+
+export default productDetails;
--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 
+import products from "./productsDetails.js";
 
 const submitBtn = document.getElementById("submit-btn");
 const validationElement = document.getElementById("validation");
@@ -23,8 +24,60 @@ validationElement.onkeyup = function(){
 // Validation
 
 
-// OnSubmit
 
 
 
-// OnSubmit
+// Product option
+
+let productSelectDocument = document.getElementById("product-category");
+
+for(const category in products){
+
+    let option = document.createElement("option");
+    option.value = category;
+    option.text = category;
+    productSelectDocument.add(option);
+}
+
+
+
+// Display Product category
+const displayProducts = ()=>{
+    let productsElement = document.getElementById("product-list");
+
+    while(productsElement.firstChild){
+        productsElement.removeChild(productsElement.firstChild);
+    }
+
+    let currentCat =  productSelectDocument.value;
+
+    for(const item in products[currentCat]){
+        
+        
+        let machineDisplay = document.createElement("div");
+        machineDisplay.className = "item";
+
+        let machineImg = document.createElement("img");
+        machineImg.src = "./products/"+currentCat+"/"+item+".png";
+
+        let nameElement = document.createElement("div");
+        nameElement.className = "name";
+        nameElement.textContent = item;
+
+
+        machineDisplay.appendChild(machineImg);
+        machineDisplay.appendChild(nameElement);
+
+        productsElement.appendChild(machineDisplay);
+    }
+}
+
+displayProducts();
+
+
+productSelectDocument.onchange = ()=>{
+    displayProducts()
+}
+
+
+// Product option
