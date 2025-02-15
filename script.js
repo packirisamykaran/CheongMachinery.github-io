1356383b37f1c4292f80e32b89335021a64bafad
Karan <packirisamykaran@gmail.com>
2025-02-15 11:09:55 +0800

    Script optimized

--- script.js
+++ script.js
@@ -1,231 +1,125 @@
-
 import products from "./productsDetails.js";
 
+// Cache DOM elements
 const submitBtn = document.getElementById("submit-btn");
 const validationElement = document.getElementById("validation");
+const productSelect = document.getElementById("product-category");
+const productsContainer = document.getElementById("product-list");
+const displayDetailElement = document.getElementById("displayItemDetails");
+const infoContainer = document.getElementById("info-container");
+const displayImgElement = document.getElementById("display-img");
+const itemDisplayCloseBtn = document.getElementById("close-item-detail");
 
-// Disable submit btn
+// Initial states
 submitBtn.disabled = true;
-
-// Disable submit btn
-
-
-
-
-// Validation
-validationElement.onkeyup = function(){
-    if(validationElement.value==="8"){
-        submitBtn.disabled = false;
-
-    }else{
-        submitBtn.disabled = true;
-    }
-}
-// Validation
-
-
-
-
-
-// Product option
-
-let productSelectDocument = document.getElementById("product-category");
-let option = document.createElement("option");
-option.value = "All";
-option.text = "All";
-productSelectDocument.add(option);
-for(const category in products){
-
-    let option = document.createElement("option");
-    option.value = category;
-    option.text = category;
-    productSelectDocument.add(option);
-}
-
-
-
-// Display Product category
-
-const displayProducts = ()=>{
-    
-    let productsElement = document.getElementById("product-list");
-
-    while(productsElement.firstChild){
-        productsElement.removeChild(productsElement.firstChild);
-    }
-
-    let currentCat =  productSelectDocument.value;
-
-    if(currentCat==="All"){
-        for(const category in products){
-            addProducts(category)
-        }
-    }
-    else{
-        addProducts(currentCat);
-    }
-
-    
-
-    
-}
-
-
-function checkFileExist(urlToFile) {
-    var xhr = new XMLHttpRequest();
-    xhr.open('HEAD', urlToFile, false);
-    xhr.send();
-     
-    if (xhr.status == "404") {
-        return false;
-    } else {
-        return true;
-    }
-}
- 
-
-
-
-const addProducts = (currentCat)=>{
-
-    let productsElement = document.getElementById("product-list");
-
-    for(const item in products[currentCat]){
-        
-        
-        let machineDisplay = document.createElement("div");
-        machineDisplay.className = "item";
-        machineDisplay.id = item;
-        machineDisplay.onclick = ()=> displayItemDetails(item, currentCat)
-
-        let machineImg = document.createElement("img");
-        machineImg.id = item;
-
-        machineImg.src = "./products/"+currentCat+"/"+item+".png";
-
-        // let pngExists = checkFileExist(machineImg.src);
-
-        if(true){
-            machineImg.src = "./products/"+currentCat+"/"+item+".png";
-        }else{
-            machineImg.src = "./products/"+currentCat+"/"+item+".jpg";
-        }
-        
-        // try {
-        //     machineImg.src = "./products/"+currentCat+"/"+item+".png";
-        // } catch (error) {
-        //     // machineImg.src = "./products/"+currentCat+"/"+item+".jgp";
-        //     console.log(error)
-        // }
-
-        // // let machineImg = new Image();
-        // machineImg.onload=function() { // when .png ok
-        //     machineImg.src = "./products/"+currentCat+"/"+item + '.png';
-        //   };
-        //   machineImg.onerror=function() { // when .png failed
-        //     machineImg.src = "./products/"+currentCat+"/"+item + '.jpg';      
-        //   };
-        //   machineImg.src="./products/"+currentCat+"/"+item + '.png'; // execute the test
-
-
-        let nameElement = document.createElement("div");
-        nameElement.className = "name";
-        nameElement.textContent = item;
-
-
-        machineDisplay.appendChild(machineImg);
-        machineDisplay.appendChild(nameElement);
-
-        productsElement.appendChild(machineDisplay);
-    }
-}
-
-
-
-displayProducts();
-
-
-productSelectDocument.onchange = ()=>{
-    displayProducts()
-}
-
-
-// Display Product category
-// Product option
-
-
-
-
-// On click display product details
-const displayDetailElement = document.getElementById("displayItemDetails")
-
-displayDetailElement.style.visibility = "hidden"
-
-const displayItemDetails = (item, currentCat)=>{
-
-    let divElement = document.getElementById("info-container");
-
-    while(divElement.firstChild){
-        divElement.removeChild(divElement.firstChild);
-    }
-
-
-    let displayImgElement = document.getElementById("display-img");
-   try {
-    displayImgElement.src = "./products/"+currentCat+"/"+item+".png"
-   } catch (error) {
-    // displayImgElement.src = "./products/"+currentCat+"/"+item+".jpg"
-    console.log("error")
-   }
-
-    let curCategory = products[currentCat];
-    let equipment = curCategory[item];
-
-
-    
-
-    
-
-    for(const detail in equipment){
-        let itemElement = document.createElement("div");
-        itemElement.className = "info-row";
-
-        let colElement = document.createElement("div");
-        colElement.classList = "key";
-        colElement.textContent = detail;
-
-        itemElement.appendChild(colElement)
-
-        colElement = document.createElement("div");
-        colElement.classList = "value";
-        colElement.textContent = equipment[detail];
-        
-        itemElement.appendChild(colElement);
-
-        divElement.appendChild(itemElement);
-        
-        console.log(detail, equipment[detail])
-
-
-    }
-    displayDetailElement.appendChild(divElement);
-
-    displayDetailElement.style.visibility = "visible"
-    displayDetailElement.style.zIndex = "1"
-
-    
-}
-
-// On click display product details
+displayDetailElement.style.visibility = "hidden";
+displayDetailElement.style.zIndex = "-1";
+
+// Validate input
+validationElement.addEventListener("keyup", () => {
+  submitBtn.disabled = validationElement.value === "8" ? false : true;
+});
 
 // Close item detail
+itemDisplayCloseBtn.addEventListener("click", () => {
+  displayDetailElement.style.visibility = "hidden";
+  displayDetailElement.style.zIndex = "-1";
+});
 
-const itemDiplayCloseBtn  = document.getElementById("close-item-detail");
-itemDiplayCloseBtn.onclick = ()=>{
-    let itemDetailDisplay = document.getElementById("displayItemDetails");
-    itemDetailDisplay.style.visibility = "hidden"
-    itemDetailDisplay.style.zIndex = "-1"
-    
+// Populate the category dropdown
+function populateCategories() {
+  const defaultOption = document.createElement("option");
+  defaultOption.value = "All";
+  defaultOption.textContent = "All";
+  productSelect.add(defaultOption);
+
+  for (const category in products) {
+    const option = document.createElement("option");
+    option.value = category;
+    option.textContent = category;
+    productSelect.add(option);
+  }
 }
 
-// close item detail
+// Clear existing child elements
+function clearChildren(element) {
+  while (element.firstChild) {
+    element.removeChild(element.firstChild);
+  }
+}
+
+// Display products based on selected category
+function displayProducts() {
+  clearChildren(productsContainer);
+
+  if (productSelect.value === "All") {
+    for (const category in products) {
+      addProductsByCategory(category);
+    }
+  } else {
+    addProductsByCategory(productSelect.value);
+  }
+}
+
+// Create and append product items to the DOM
+function addProductsByCategory(category) {
+  const categoryProducts = products[category];
+
+  for (const itemName in categoryProducts) {
+    const productWrapper = document.createElement("div");
+    productWrapper.className = "item";
+    productWrapper.id = itemName;
+    productWrapper.addEventListener("click", () => {
+      displayItemDetails(itemName, category);
+    });
+
+    const productImg = document.createElement("img");
+    productImg.id = itemName;
+    productImg.src = `./products/${category}/${itemName}.png`;
+
+    const nameElement = document.createElement("div");
+    nameElement.className = "name";
+    nameElement.textContent = itemName;
+
+    productWrapper.appendChild(productImg);
+    productWrapper.appendChild(nameElement);
+    productsContainer.appendChild(productWrapper);
+  }
+}
+
+// Display detailed info of a clicked product
+function displayItemDetails(itemName, category) {
+  clearChildren(infoContainer);
+
+  try {
+    displayImgElement.src = `./products/${category}/${itemName}.png`;
+  } catch (error) {
+    console.log("Error loading .png file:", error);
+  }
+
+  const itemDetails = products[category][itemName];
+  for (const detail in itemDetails) {
+    const row = document.createElement("div");
+    row.className = "info-row";
+
+    const keyElement = document.createElement("div");
+    keyElement.className = "key";
+    keyElement.textContent = detail;
+
+    const valueElement = document.createElement("div");
+    valueElement.className = "value";
+    valueElement.textContent = itemDetails[detail];
+
+    row.appendChild(keyElement);
+    row.appendChild(valueElement);
+    infoContainer.appendChild(row);
+  }
+
+  displayDetailElement.style.visibility = "visible";
+  displayDetailElement.style.zIndex = "1";
+}
+
+// Initialize dropdown and display
+populateCategories();
+productSelect.addEventListener("change", displayProducts);
+displayProducts();
